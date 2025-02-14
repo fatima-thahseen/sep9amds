6d50970412d1ba0da52981e423638a7260f5013a
fatima-thahseen <fatimma.thahseen@gmail.com>
2025-02-14 10:08:58 +0530

    msg

--- data.py
+++ data.py
@@ -1,7 +1,9 @@
 def reverse_string(s):
-    return s[::-1]
+    return s[::-1]if s else "You entered an empty string!"
+
 
 # Taking user input
+print(" Welcome to the String Reverser Program! ")
 text = input("Enter a string: ")
 print("Reversed:", reverse_string(text))
 
